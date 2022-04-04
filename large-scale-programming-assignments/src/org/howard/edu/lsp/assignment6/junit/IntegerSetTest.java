15a1eef9b1da05d808059fa0d15e5221cbd37345
Javid Mitchell <98503376+javidt16@users.noreply.github.com>
2022-04-04 01:10:22 -0400

    Completed Junit tests

--- large-scale-programming-assignments/src/org/howard/edu/lsp/assignment6/junit/IntegerSetTest.java
+++ large-scale-programming-assignments/src/org/howard/edu/lsp/assignment6/junit/IntegerSetTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.howard.edu.lsp.assignment6.integerset.IntegerSet;
+import org.howard.edu.lsp.assignment6.integerset.IntegerSetException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -147,5 +148,375 @@ public class IntegerSetTest {
 
 	}
 	
+	@Test
+	@DisplayName("Test Case for if the set contains the value")
+	public void testLargest() {
+		testSet1.add(1);
+		testSet1.add(2);
+		testSet1.add(3);
+		testSet1.add(4);
+		assertTrue(testSet1.contains(3));
+
+	}
+	
+	@Test
+	@DisplayName("Test Case for Largest Method")
+	public void testLargestMethod1() {
+		testSet1.add(1);
+		testSet1.add(2);
+		testSet1.add(3);
+		testSet1.add(4);
+		testSet1.add(5);
+		int largest_num = 0;
+		
+		try {
+			largest_num = testSet1.largest();
+		}
+		catch (IntegerSetException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		assertTrue(largest_num == 5);
+	};
+	
+	@Test
+	@DisplayName("Test Case for Largest Method with equal numbers in integerSet")
+	public void testLargest2() {
+		testSet1.add(1);
+		testSet1.add(1);
+
+		int largest_num = 0;
+		
+		try {
+			largest_num = testSet1.largest();
+		}
+		catch (IntegerSetException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		assertTrue(largest_num == 1);
+	};
+	
+	@Test
+	@DisplayName("Test Case for Largest Method with negative numbers")
+	public void testLargest3() {
+		testSet1.add(-1);
+		testSet1.add(-6);
+
+		int largest_num = 0;
+		
+		try {
+			largest_num = testSet1.largest();
+		}
+		catch (IntegerSetException e) {
+			System.out.println(e.getMessage());
+		}
+
+		assertTrue(largest_num == 0);
+	};
+	
+	@Test
+	@DisplayName("Test Case for Largest Method on empty integerSet")
+	public void testLargest4() {
+		int largest_num = 0;
+		
+		try {
+			largest_num = testSet1.largest();
+		}
+		catch (IntegerSetException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		assertTrue(largest_num == 0);
+	};
+	
+	@Test
+	@DisplayName("Test Case for Smallest Method")
+	public void testSmallest1() {
+		testSet1.add(1);
+		testSet1.add(2);
+		testSet1.add(3);
+		testSet1.add(4);
+		testSet1.add(5);
+		int smallest_num = 0;
+		
+		try {
+			smallest_num = testSet1.smallest();
+		}
+		catch (IntegerSetException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		
+		assertTrue(smallest_num == 1);
+	};
+	
+	@Test
+	@DisplayName("Test Case for Smallest Method with equal numbers in integerSet")
+	public void testSmallest2() {
+		testSet1.add(5);
+		testSet1.add(5);
+		int smallest_num = 0;
+		
+		try {
+			smallest_num = testSet1.smallest();
+		}
+		catch (IntegerSetException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		assertTrue(smallest_num == 5);
+	};
+	
+	@Test
+	@DisplayName("Test Case for Smallest Method with negative numbers")
+	public void testSmallest3() {
+		testSet1.add(-4);
+		testSet1.add(-5);
+		int smallest_num = 0;
+		
+		try {
+			smallest_num = testSet1.smallest();
+		}
+		catch (IntegerSetException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		assertTrue(smallest_num == -5);
+	};
+	
+	@Test
+	@DisplayName("Test Case for Add Method")
+	public void testAdd1() {
+		testSet1.add(2);
+		assertFalse(testSet1.isEmpty());
+	};
+	
+	@Test
+	@DisplayName("Test Case for add with String instead of integer")
+	public void testAdd2() {
+		testSet1.add('s');
+		assertFalse(testSet1.isEmpty());
+	};
+	
+	@Test
+	@DisplayName("Test Case for Remove Method")
+	public void testRemove() {
+		testSet1.add(1);
+		testSet1.add(2);
+		testSet1.add(3);
+		testSet1.add(4);
+		testSet1.add(5);
+		
+		testSet2.add(1);
+		testSet2.add(2);
+		testSet2.add(3);
+		testSet2.add(4);
+		testSet2.add(5);
+		
+		testSet2.remove(5);
+		
+		
+		assertFalse(testSet1.equals(testSet2));
+	};
+	
+	@Test
+	@DisplayName("Test Case for Remove duplicates")
+	public void testRemove2() {
+		testSet1.add(1);
+		testSet1.add(2);
+		testSet1.add(3);
+		
+		testSet2.add(1);
+		testSet2.add(2);
+		testSet2.add(3);
+		testSet2.add(4);
+		testSet2.add(4);
+		
+		testSet2.remove(4);
+		
+		
+		assertTrue(testSet1.equals(testSet2));
+	};
+	
+	@Test
+	@DisplayName("Test Case for Removeing a number not in integerSet")
+	public void testRemove3() {
+		testSet1.add(1);
+		testSet1.add(2);
+		testSet1.add(3);
+		
+		testSet2.add(1);
+		testSet2.add(2);
+		testSet2.add(3);
+		
+		testSet2.remove(6);
+		
+		
+		assertTrue(testSet1.equals(testSet2));
+	};
+	
+	@Test
+	@DisplayName("Test Case 1 for Union Method")
+	public void testUnion1() {
+		testSet1.add(9);
+		testSet1.add(3);
+		testSet1.add(5);
+		testSet1.add(4);
+		
+		testSet2.add(9);
+		testSet2.add(3);
+		testSet2.add(12);
+		testSet2.add(34);
+		
+		testSet1.union(testSet2);
+		
+		testSet3.add(9);
+		testSet3.add(3);
+		testSet3.add(5);
+		testSet3.add(4);
+		testSet3.add(12);
+		testSet3.add(34);
+		
+		assertTrue(testSet1.equals(testSet3));
+	};
+	
+	@Test
+	@DisplayName("Test Case 2 for Union Method with two equal integerSets")
+	public void testUnion2() {
+		testSet1.add(5);
+		testSet1.add(4);
+		
+		testSet2.add(5);
+		testSet2.add(4);
+		
+		testSet1.union(testSet2);
+		
+		testSet3.add(5);
+		testSet3.add(4);
+
+		assertTrue(testSet1.equals(testSet3));
+	};
+	
+	@Test
+	@DisplayName("Test Case for Union Method with an empty and a non empty integerSet")
+	public void testUnion3() {
+		testSet1.add(5);
+		testSet1.add(4);
+		
+		testSet1.union(testSet2);
+		
+		testSet3.add(5);
+		testSet3.add(4);
+
+		assertTrue(testSet1.equals(testSet3));
+	};
+	
+	
+	
+	@Test
+	@DisplayName("Test Case for Intersect Method")
+	public void testIntersect() {
+		testSet1.add(9);
+		testSet1.add(3);
+		testSet1.add(5);
+		testSet1.add(4);
+		
+		
+		testSet2.add(9);
+		testSet2.add(3);
+		testSet2.add(12);
+		testSet2.add(34);
+		
+		testSet1.intersect(testSet2);
+		
+		testSet3.add(9);
+		testSet3.add(3);
+		
+		assertTrue(testSet1.equals(testSet3));
+	};
+	
+	@Test
+	@DisplayName("Test Case for Intersect Method between two integerSets with no intersection")
+	public void testIntersect2() {
+		testSet1.add(5);
+		testSet1.add(4);
+		
+		
+		testSet2.add(12);
+		testSet2.add(34);
+		
+		testSet1.intersect(testSet2);
+		
+		assertTrue(testSet1.equals(testSet3));
+	};
+	
+	@Test
+	@DisplayName("Test Case for Difference Method")
+	public void testDiff() {
+		testSet1.add(9);
+		testSet1.add(3);
+		testSet1.add(5);
+		testSet1.add(4);
+		
+		
+		testSet2.add(9);
+		testSet2.add(3);
+		testSet2.add(12);
+		testSet2.add(34);
+		
+		testSet1.diff(testSet2);
+		
+		testSet3.add(5);
+		testSet3.add(4);
+		
+		assertTrue(testSet1.equals(testSet3));
+	};
+	
+	@Test
+	@DisplayName("Test Case for Difference Method on integerSets with no common integers")
+	public void testDiff2() {
+		testSet1.add(9);
+		testSet1.add(3);
+		testSet1.add(5);
+		testSet1.add(4);
+		
+		
+		testSet2.add(12);
+		testSet2.add(34);
+		
+		testSet1.diff(testSet2);
+		
+		testSet3.add(9);
+		testSet3.add(3);
+		testSet3.add(5);
+		testSet3.add(4);
+		
+		assertTrue(testSet1.equals(testSet3));
+	};
+	
+	@Test
+	@DisplayName("Test Case for toString Method")
+	public void testString1() {
+		testSet1.add(9);
+		testSet1.add(3);
+		testSet1.add(5);
+		testSet1.add(4);
+		
+		String str1 = testSet1.toString();
+		String str2 = "[9, 3, 5, 4]";
+		
+		assertTrue(str1.equals(str2));
+	}
+	
+	@Test
+	@DisplayName("Test Case for toString Method on empty integerSet")
+	public void testString2() {
+		
+		String str1 = testSet1.toString();
+		String str2 = "[]";
+		
+		assertTrue(str1.equals(str2));
+	}
 }
 
