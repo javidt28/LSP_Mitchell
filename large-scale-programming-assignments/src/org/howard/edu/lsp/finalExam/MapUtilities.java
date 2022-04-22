6965c608a3b6bec4cefc23c61ead2308f62dbac4
Javid Mitchell <98503376+javidt16@users.noreply.github.com>
2022-04-22 02:40:56 -0400

    Final Exam

--- large-scale-programming-assignments/src/org/howard/edu/lsp/finalExam/MapUtilities.java
+++ large-scale-programming-assignments/src/org/howard/edu/lsp/finalExam/MapUtilities.java
@@ -1,12 +1,22 @@
 package org.howard.edu.lsp.finalExam;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class MapUtilities {
-    public static int commonKeyValuePairs(
-        HashMap<String, String> map1,
-        HashMap<String, String> map2)throws NullMapException {
-			return 0;
-// implementation
-    }
+        public static int commonKeyValuePairs(HashMap<String, String> map1, HashMap<String, String> map2) throws NullMapException {
+                int i=0;
+                if(map1==null || map2==null)
+                        throw new NullMapException("One of both maps are null!");
+                for (Map.Entry<String, String> set :
+                        map1.entrySet()) {
+                        if(map2.containsKey(set.getKey()))
+                        {
+                        	if(set.getValue().equals(map2.get(set.getKey())))
+                        		i++;
+                        }
+                }
+                return i;
+        }
 }
+
---
+++ large-scale-programming-assignments/src/org/howard/edu/lsp/finalExam/junit/MapUtilitiesTest.java
@@ -0,0 +1,56 @@
+package org.howard.edu.lsp.finalExam.junit;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+
+import org.howard.edu.lsp.finalExam.MapUtilities;
+import org.howard.edu.lsp.finalExam.NullMapException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class MapUtilitiesTest {
+
+        @Test
+    	@DisplayName("Testing with two HashMaps for common key value pair")
+        void testCommonKeyValuePairs() throws NullMapException {
+                HashMap<String, String> map1=new HashMap<String, String>();
+                map1.put("Alice", "Healthy");
+                map1.put("Mary", "Ecastic");
+                map1.put("Bob", "Happy");
+                map1.put("Chuck", "Fine");
+                map1.put("Felix", "Sick");
+                
+                HashMap<String, String> map2=new HashMap<String, String>();
+                map2.put("Mary" , "Ecastic");
+                map2.put("Felix", "Healthy");
+                map2.put("Ricardo", "Subperb");
+                map2.put("Tam", "Fine");
+                map2.put("Bob", "Happy");
+
+                assertEquals(2, MapUtilities.commonKeyValuePairs(map1, map2));
+        }
+        
+        @Test
+    	@DisplayName("Testing with one null HashMap")
+        void testCommonKeyValuePairs2() {
+                //Create map1
+                HashMap<String, String> map1=new HashMap<String, String>();
+                map1.put("Alice", "Healthy");
+                map1.put("Mary", "Ecastic");
+                map1.put("Bob", "Happy");
+                map1.put("Chuck", "Fine");
+                map1.put("Felix", "Sick");
+                HashMap<String, String> map2=null;
+                int count=0;
+                try {
+                        count=MapUtilities.commonKeyValuePairs(map1, map2);
+                }
+                catch(Exception e) {
+                        assertEquals("One of both maps are null!", e.getMessage());
+                        assertEquals(0, count);
+                }
+        }
+
+
+}
